3a94665fe7665027ee92474a07785beaf7270ac4
geetha1997dj <109265630+geetha1997dj@users.noreply.github.com>
2022-08-09 12:25:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('this is a demo of g');
+  res.send('this is a demo of geetha');
 });
 
 app.listen(PORT, HOST);
